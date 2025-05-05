cdfa588fd8614fa8384270b7156c7dc7d8dbe6be
nicocanela <nicolascanales@ug.uchile.cl>
2025-05-05 12:29:09 -0400

    frontend build

--- part3/demo/index.js
+++ part3/demo/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 let notes = [
